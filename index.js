b8c431f54078eb61afe185b46b52b9a9c9a5a128
SylviaifyX <ifeomasylviaokpara@gmail.com>
2023-09-07 15:21:10 +0100

    webPage

---
+++ index.js
@@ -0,0 +1,20 @@
+let todayDate = document.getElementById("date")
+let daysofWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let millisecTime = document.getElementById("milliseconds")
+
+//to get today date start here//
+let newdate = new Date();
+let updateDay = newdate.getDay();
+todayDate.innerHTML=`Today is: ${daysofWeek[updateDay]}`;
+// todayDate.innerHTML = (daysofWeek[updateDay]);
+// console.log(todayDate);
+// console.log(updateDay);
+//to get today date ends here//
+
+/* to get MilliSeconds*/
+
+let MilliDate = new Date();
+let newMill = MilliDate.getMilliseconds();
+
+millisecTime.innerHTML = newMill;
+console.log(millisecTime)
