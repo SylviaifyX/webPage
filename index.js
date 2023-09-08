67dd64c8c77d1df053a711135fc83d387b5802da
SylviaifyX <ifeomasylviaokpara@gmail.com>
2023-09-08 17:22:56 +0100

    updated webpage

--- index.js
+++ index.js
@@ -6,25 +6,12 @@ let millisecTime = document.getElementById("milliseconds")
 let newdate = new Date();
 let updateDay = newdate.getUTCDay();
 todayDate.innerHTML = `${daysofWeek[updateDay]}`;
-// todayDate.innerHTML = (daysofWeek[updateDay]);
-// console.log(todayDate);
-// console.log(updateDay);
-
 //to get today date ends here//
 
+setInterval(() =>{
+    millisecTime.innerHTML = Date.now();
+},100);
 
-/* to get MilliSeconds*/
 
-setInterval(() => {
-    let MilliDate =  new Date();
-    let newMill = MilliDate.getTime();
-    millisecTime.innerHTML = newMill;
-    // console.log(millisecTime)
-},);
 
-// setInterval(() =>{
-//     millisecTime.innerHTML = Date.now();
-//     console.log(millisecTime)
-// },100);
 
-/* to get MilliSeconds ends*/
